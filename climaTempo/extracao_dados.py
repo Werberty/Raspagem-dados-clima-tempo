0fc60c6c08b4d5ca22906ae797a5bace54377808
Werberty Alexandre <werberty.alexandre@gmail.com>
2022-03-15 11:24:01 -0300

    Raspando principais dados

--- climaTempo/extracao_dados.py
+++ climaTempo/extracao_dados.py
@@ -1,3 +1,6 @@
+from lib2to3.pgen2 import driver
+from bs4 import BeautifulSoup
+import requests
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
@@ -10,9 +13,30 @@ import time
 # navegador = webdriver.Chrome(options=chrome_options)
 
 navegador = webdriver.Chrome()
-navegador.get('https://www.climatempo.com.br/previsao-do-tempo/cidade/1000/varzeaalegre-ce')
+url = 'https://www.climatempo.com.br/previsao-do-tempo/cidade/1000/varzeaalegre-ce'
+navegador.get(url)
 
+# temp_min = navegador.find_element(
+#     by=By.XPATH, value='//*[@id="min-temp-1"]').text
+# temp_max = navegador.find_element(
+#     by=By.XPATH, value='//*[@id="max-temp-1"]').text
+temperatura = navegador.find_element(
+    by=By.XPATH, value='//ul[@class="variables-list"]/li[1]/div').text
+chuva = navegador.find_element(
+    by=By.XPATH, value='//ul[@class="variables-list"]/li[2]/div').text
+vento = navegador.find_element(
+    by=By.XPATH, value='//ul[@class="variables-list"]/li[3]/div').text
+umidade = navegador.find_element(
+    by=By.XPATH, value='//ul[@class="variables-list"]/li[4]/div').text
+sol = navegador.find_element(
+    by=By.XPATH, value='//ul[@class="variables-list"]/li[5]/span[2]').text
 
+# print(temp_min, temp_max, chuva, vento)
 
-time.sleep(4)
+print(temperatura)
+print(chuva)
+print(vento)
+print(umidade)
+print(sol)
+
 navegador.quit()
